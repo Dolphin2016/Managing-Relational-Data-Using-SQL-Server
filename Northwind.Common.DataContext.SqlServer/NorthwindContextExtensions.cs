7630a2785ed52e8abca34db76d3a7799f33548db
Dolphin2016 <evgenij55555@gmail.com>
2023-05-05 16:04:57 +0500

    Creating a Class for the Data Context

---
+++ Northwind.Common.DataContext.SqlServer/NorthwindContextExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Packt.Shared;
+
+public static class NorthwindContextExtensions
+{
+    /// <summary>
+    /// Adds NorthwindContext to the specified IServiceCollection. Uses the SqlServer database provider.
+    /// </summary>
+    /// <param name="services"></param>
+    /// <param name="connectionString">Set to override the default.</param>
+    /// <returns>An IServiceCollection that can be used to add more services.</returns>
+
+
+    public static IServiceCollection AddNorthwindContext(
+        this IServiceCollection services,
+        string connectionString = "Data Source=eugene-pc\\dev;Initial Catalog=Northwind;" + "Integrated Security=true;MultipleActiveResultsets=true;Encrypt=false")
+    {
+        services.AddDbContext<NorthwindContext>(options =>
+        {
+            options.UseSqlServer(connectionString);
+            options.LogTo(WriteLine,
+                new[] {
+                    Microsoft.EntityFrameworkCore
+                .Diagnostics.RelationalEventId.CommandExecuting });
+        });
+
+        return services;
+    }
+}
+
+
+
