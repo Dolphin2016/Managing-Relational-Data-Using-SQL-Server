d1ecc062f92c88be2edd32a21d2ce5e54761e283
Dolphin2016 <evgenij55555@gmail.com>
2023-05-05 15:03:40 +0500

    Table-Per-Type (TPT) Mapping Strategy

--- Northwind.Console.HierarchyMapping/HierarchyDb.cs
+++ Northwind.Console.HierarchyMapping/HierarchyDb.cs
@@ -20,7 +20,8 @@ public class HierarchyDb : DbContext
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Person>()
-            .UseTphMappingStrategy();
+            //.UseTphMappingStrategy();
+            .UseTptMappingStrategy();
 
         // Populate database with sample data.
 
